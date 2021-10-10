1573a195a6ebfe7b8b58f6eeb5b2e70067f53d75
YunJiSang <31590576+beargrllys@users.noreply.github.com>
2021-10-10 22:56:18 +0900

    eee

--- SideProject/capstone/connect.py
+++ SideProject/capstone/connect.py
@@ -1,37 +1,55 @@
 import mariadb
 import sys
 import datetime
+import threading
 
-# Connect to MariaDB Platform
-try:
-    conn = mariadb.connect(
-        user="raspi_user",
-        password="wlgkcjf21gh",
-        host="112.170.208.72",
-        port=20,
-        database="rasp_db"
+#화분 내부 네트워크 코드입니다.
 
-    )
-except mariadb.Error as e:
-    print(f"Error connecting to MariaDB Platform: {e}")
-    sys.exit(1)
+class NetworkSession:
+    conn = mariadb
+    #
+    def __init__(self):# 네트워크를 초기화합니다.
+        # Connect to MariaDB Platform
+        try:
+            self.conn = mariadb.connect(
+                user="raspi_user",
+                password="wlgkcjf21gh",
+                host="112.170.208.72",
+                port=20,
+                database="rasp_db"
+                )
+            print("DataBase Connected")
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
 
-# Get Cursor
-cur = conn.cursor()
-#some_name = "Georgi"
-#cur.execute("SELECT sensor,PW FROM TESTTABLE WHERE NAME=?", (some_name,))
+    def __del__(self):
+        self.conn.close()
+        print("DataBase Session Closed")
+    
+    def insert_DB(self, plantID ,plantName, Temper , Humid ,waterLevel , waterSpeed ,soil, CDS):#화분에 수집된 센서 값을 데이터베이스에 올립니다.
+        # Get Cursor
+        cur = self.conn.cursor()
 
-# for first_name, last_name in cur:
-#    print(f"NAME: {first_name}, PW: {last_name}")
+        try:
+            cur.execute("INSERT INTO collect_data (plantID ,plantName,collect_time, Temper , Humid ,waterLevel , waterSpeed ,soil, CDS) VALUES (?,?,?,?,?,?,?,?,?)",
+                        (plantID ,plantName, datetime.datetime.now(), Temper , Humid ,waterLevel , waterSpeed ,soil, CDS))
+        except mariadb.Error as e:
+            print(f"Error: {e}")
 
-# insert information
-try:
-    cur.execute("INSERT INTO collect_data (sensor,collect_time,value1, value2) VALUES (?,?,?,?)",
-                ("testSensor", datetime.datetime.now(), "0.1", "2.1"))
-except mariadb.Error as e:
-    print(f"Error: {e}")
+        self.conn.commit()
+        threading.Timer(5, self.insert_DB(plantID ,plantName, Temper , Humid ,waterLevel , waterSpeed ,soil, CDS))
 
-conn.commit()
-print(f"Last Inserted ID: {cur.lastrowid}")
+    def get_OrderData(self, Order):#화분에 내릴 명령을 구분합니다.
+        cur = self.conn.cursor()
 
-conn.close()
+        some_name = "Georgi"
+        cur.execute("SELECT sensor,PW FROM TESTTABLE WHERE NAME=?", (some_name,))
+
+        for first_name, last_name in cur:
+            print(f"NAME: {first_name}, PW: {last_name}")
+
+
+
+
+
