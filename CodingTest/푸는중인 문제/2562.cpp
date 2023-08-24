1b13500ed0aab23e670b2c4b2d57cbdee0371cd4
YunJiSang <31590576+beargrllys@users.noreply.github.com>
2023-08-24 22:40:57 +0900

    2023.08.24

--- CodingTest/푸는중인 문제/2562.cpp
+++ CodingTest/푸는중인 문제/2562.cpp
@@ -1,7 +1,24 @@
 //2023.08.21
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int val = -1, idx = -1;
 
 int main(){
-	
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	int tmp;
+	for(int i = 0; i < 9; i++){
+		cin >> tmp;
+		if(val < tmp)
+		{
+			val = tmp;
+			idx = i+1;
+		}
+	}
+
+	cout << val << "\n" << idx;
 }
